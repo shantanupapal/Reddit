d158b39fc3ac8a2996378df81272d68a08bece9c
madhuridv <77411174+madhuridv@users.noreply.github.com>
2021-05-28 11:14:28 -0700

    Update awsImageUpload.js

--- backend/utils/awsImageUpload.js
+++ backend/utils/awsImageUpload.js
@@ -1,7 +1,7 @@
 "use strict";
 const awsConfig = {
-  AWS_ID: "AKIA5SCXGSD2C4YPE3LD",
-  AWS_SECRET: "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",
+  AWS_ID: "",
+  AWS_SECRET: "",
   AWS_BUCKET_NAME: "redditbucket12",
   awsPermission: "public-read",
 };
