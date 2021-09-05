2c020eb6e1905cd7ea5dcaa14928a330006a2992
shantanupapal <40557046+shantanupapal@users.noreply.github.com>
2021-09-04 20:45:00 -0700

    Update config.js

--- kafka-backend/utils/config.js
+++ kafka-backend/utils/config.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
     mongoDBURI:
-        "mongodb+srv://admin:admin@cluster0.57tzh.mongodb.net/reddit?retryWrites=true&w=majority",
+        "",
     kafkaURI: "localhost:2181",
     redisHost: "localhost",
     redisPort: 6379,
