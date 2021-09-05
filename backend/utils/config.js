ab54584410d3ecfc66188e175423dcf37ec21023
shantanupapal <40557046+shantanupapal@users.noreply.github.com>
2021-09-04 20:40:50 -0700

    Update config.js

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -3,10 +3,10 @@ const config = {
 	secret: "cmpe273_kafka_passport_mongo",
 	frontendURI: "http://localhost:3000",
 	kafkaURI: "localhost:2181",
-	mysqlUser: "admin",
-	mysqlPassword: "adminpayal",
-	mysqlHost: "database-ms1.cnqtrxygyjza.us-west-1.rds.amazonaws.com",
-	mysqlDatabase: "dbReddit",
+	mysqlUser: "",
+	mysqlPassword: "",
+	mysqlHost: "",
+	mysqlDatabase: "",
 };
 
 module.exports = config;
